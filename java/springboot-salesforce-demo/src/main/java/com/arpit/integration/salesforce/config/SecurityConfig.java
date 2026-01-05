fa013d3f13452511268ced53ca9794b883712197
Arpit Ajmera <ajmeraarpit1024@gmail.com>
2026-01-05 17:57:16 +0530

    Relax Spring Security for local development

--- java/springboot-salesforce-demo/src/main/java/com/arpit/integration/salesforce/config/SecurityConfig.java
+++ java/springboot-salesforce-demo/src/main/java/com/arpit/integration/salesforce/config/SecurityConfig.java
@@ -11,8 +11,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-        http
-                .csrf(csrf -> csrf.disable())
+        http.csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .anyRequest().permitAll()
                 )
